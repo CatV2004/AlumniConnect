76e8a1c263927c023231b29bff1b163a7612e959
CatV2004 <nguyenmanhc261@gmail.com>
2025-05-18 12:45:42 +0700

    template

---
+++ backend/AlumniConnect/src/main/java/com/cmc/repository/InvitationPostRepository.java
@@ -0,0 +1,29 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.cmc.repository;
+//
+//import com.cmc.pojo.InvitationPost;
+//import java.util.List;
+//
+///**
+// *
+// * @author FPTSHOP
+// */
+//public interface InvitationPostRepository {
+//
+//    InvitationPost findById(Long id);
+//
+//    List<InvitationPost> findAll();
+//
+//    void save(InvitationPost post);
+//
+//    void update(InvitationPost post);
+//
+//    void delete(Long id);
+//
+//    List<InvitationPost> findByGroupId(Long groupId);
+//
+//    List<InvitationPost> findByUserId(Long userId);
+//}
